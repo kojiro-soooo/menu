c50666c6c4d6e71f7e577fc47708e6b94ba88277
Kojiro So <90157622+kojiro-soooo@users.noreply.github.com>
2023-10-28 03:25:53 -0400

    Revert "fix: add AWS credentials to environment file" (#6)

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -63,7 +63,7 @@ const Create = () => {
     
 
     // create a new recipe document in database
-    await fetch(`/create`, { //when testing locally, remove the url part of ${backend_url}/create
+    await fetch(`backend_url/create`, { //when testing locally, remove the url part of ${backend_url}/create
       method: "POST",
     //   headers: {
     //     "Content-Type": "multipart/form-data",
