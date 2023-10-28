babdc3e1e16b909739aea7faba8daf97bd258ba4
Kojiro So <kojiro.soooo@gmail.com>
2023-10-28 14:48:36 -0400

    refactor: edit cors policy

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 connectDB();
 
 // cors: enables requests from all sources
-app.use(cors({origin: true}));
+app.use(cors());
 
 // parse incoming requests as JSON, making data readily available via req.body
 app.use(express.json());
@@ -29,6 +29,6 @@ app.use(express.json());
 // API
 app.use('/',router);
 
-app.listen(process.env.PORT || 5050, () => {
+app.listen(process.env.PORT || 5050, () => { 
   console.log("Server is running at port 5050");
 });
