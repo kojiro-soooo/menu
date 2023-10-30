55e60b2797da051eaff069af2277939985483c96
Kojiro So <kojiro.soooo@gmail.com>
2023-10-30 00:51:26 -0400

    fix: add client ID for production

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
             <Auth0Provider
                 domain="dev-c0dwltendwxbev8d.us.auth0.com"
-                clientId="hV55iuMCaQkohRHQwujZdQanluWbNssp"
+                clientId="nCEx1jEbQTgFbqzI1YrOtCfhfVvu74bU"
+                // clientId="hV55iuMCaQkohRHQwujZdQanluWbNssp"
                 authorizationParams={{
                     redirect_uri: window.location.origin
                 }}
