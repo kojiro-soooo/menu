21f6db32304c41a08fa5c4def40c71d0d0067243
Kojiro So <kojiro.soooo@gmail.com>
2023-10-30 00:42:07 -0400

    feat: integrate auth0 for user auth and add login, logout, and profile buttons

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+            <Auth0Provider
+                domain="dev-c0dwltendwxbev8d.us.auth0.com"
+                clientId="hV55iuMCaQkohRHQwujZdQanluWbNssp"
+                authorizationParams={{
+                    redirect_uri: window.location.origin
+                }}
+            >
+                <App />
+            </Auth0Provider>,
 );
 
