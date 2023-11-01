2761229d5284767c04c25d40320414e0ae908c05
Kojiro So <kojiro.soooo@gmail.com>
2023-11-01 14:17:58 -0400

    fix: keep users from creating recipes without images

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -107,6 +107,7 @@ const Create = ( {userId, isAuthenticated, isLoading} ) => {
                     type="file"
                     id="file-input"
                     name="ImageStyle"
+                    required
                 ></input>
                 <label>Recipe Name: </label>
                 <input
