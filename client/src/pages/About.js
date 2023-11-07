99083f1787b75a979f4aede284303ed804cde7b5
Kojiro So <kojiro.soooo@gmail.com>
2023-11-06 19:21:41 -0500

    feat: update about page content

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -14,12 +14,14 @@ const About = () => {
       </div>
       <div className="about__content">
         <p>
-          At Authentic Recipes, we understand the power of authentic, home-made
-          food in connecting people with their cultural roots. As migrants
+          At meNu, we understand the power of authentic, home-made
+          food in connecting people with each other and their cultural roots. As migrants
           ourselves, we have a deep appreciation for the flavors and traditions
           that remind us of the places we've called home: Japan, India,
           Australia, the USA, and beyond.
         </p>
+        <br></br>
+
         <p>
           Our mission is to create a modern and polished platform where food
           enthusiasts, like yourself, can come together to explore and share
@@ -29,7 +31,7 @@ const About = () => {
         </p>
         <h2> Browse Recipes from Every Country</h2>
         <p>
-          We have curated an extensive collection of recipes from diverse
+          Users have created an extensive collection of recipes from diverse
           cultures around the globe. Whether you're searching for traditional
           Italian pasta, spicy Thai curry, or hearty Mexican tacos, our platform
           is your gateway to discovering culinary delights from all over the
@@ -45,7 +47,22 @@ const About = () => {
           recipes, you'll become part of a global community that appreciates the
           art of cooking and sharing flavors from different cultures.
         </p>
-        <h2>Vote for Authenticity</h2>
+        <h2>Stay Tuned For More</h2>
+        <p> We understand the importance of authenticity in preserving culinary
+          traditions. We will be implementing a voting system where users can
+          express their agreement on the authenticity of a recipe. This ensures
+          that the most genuine and true-to-roots recipes rise to the top,
+          creating a reliable resource for users seeking an authentic taste
+          experience.</p>
+          <br></br>
+          <p>To make your cooking experience even more convenient, we will be offering a
+          direct purchasing feature. When you discover a recipe that requires
+          unique or hard-to-find ingredients, we will provide a convenient option to
+          purchase those items directly through our website. No more scouring
+          multiple stores or specialty markets—get everything you need delivered
+          to your doorstep, so you can focus on what you love most: creating
+          delicious meals.</p>
+        {/* <h2>Vote for Authenticity</h2>
         <p>
           We understand the importance of authenticity in preserving culinary
           traditions. We have implemented a voting system where users can
@@ -53,8 +70,8 @@ const About = () => {
           that the most genuine and true-to-roots recipes rise to the top,
           creating a reliable resource for users seeking an authentic taste
           experience.
-        </p>
-        <h2>Find Hard-to-Find Ingredients</h2>
+        </p> */}
+        {/* <h2>Find Hard-to-Find Ingredients</h2>
         <p>
           To make your cooking experience even more convenient, we offer a
           direct purchasing feature. When you discover a recipe that requires
@@ -63,9 +80,10 @@ const About = () => {
           multiple stores or specialty markets—get everything you need delivered
           to your doorstep, so you can focus on what you love most: creating
           delicious meals.
-        </p>
+        </p> */}
+        <h2>Join Us On Our Journey</h2>
         <p>
-          Join us at Authentic Recipes and become part of a global community
+          Join us at meNu and become part of a global community
           that celebrates culinary diversity and the joy of sharing food. Let's
           connect through flavors, traditions, and the universal language of
           taste. Together, we can savor the world, one recipe at a time.
