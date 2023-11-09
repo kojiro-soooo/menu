c9fd22d419b640ed5d7f2335aa31c9e20cc26aa6
Kojiro So <kojiro.soooo@gmail.com>
2023-11-09 17:52:59 -0500

    refactor: hide client ID for auth0 in environment variable

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
             <Auth0Provider
                 domain="dev-c0dwltendwxbev8d.us.auth0.com"
                 cacheLocation='localstorage' // enables storing auth status in localstorage so it doesn't have to be retrieved on refresh
-                clientId="nCEx1jEbQTgFbqzI1YrOtCfhfVvu74bU" // prod
-                // clientId="hV55iuMCaQkohRHQwujZdQanluWbNssp" // staging
+                // clientId={process.env.REACT_APP_CLIENT_ID_PRODUCTION} // prod
+                clientId={process.env.REACT_APP_CLIENT_ID_STAGING} // staging
                 authorizationParams={{
                     redirect_uri: window.location.origin,
                     // audience: "https://dev-c0dwltendwxbev8d.us.auth0.com/api/v2/",
