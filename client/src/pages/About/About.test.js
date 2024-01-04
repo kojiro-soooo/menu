bd9ced0c2847fb92736a5032e1497f886734643e
Kojiro So <90157622+kojiro-soooo@users.noreply.github.com>
2024-01-03 20:34:15 -0500

    Refactor about unit test (#21)
    * refactor: add unit tests to all non-page components
    
    More unit tests to come, but this will add some coverage to non-page components for now.
    
    * refactor: move component files to respective folders
    
    * refactor: delete old files from main
    
    * refactor: update paths in App.tsx
    
    * refactor: update filepaths
    
    * refactor: delete unnecessary unit test

--- client/src/pages/About/About.test.js
+++ client/src/pages/About/About.test.js
@@ -5,15 +5,7 @@ import About from './About';
 describe('About', () => {
     window.scrollTo = jest.fn();
 
-
     it('renders About', () => {
         render(<About />);
-        // screen.debug();
-    });
-
-    it('renders title', () => {
-        render(<About />);
-        const headingElement = screen.getByText(/Our Story/i);
-        expect(headingElement).toBeInTheDocument();
     });
 });
