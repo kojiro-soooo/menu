0e064ce333496893c880053483520f7cd5259b4e
Kojiro So <90157622+kojiro-soooo@users.noreply.github.com>
2024-01-05 18:53:21 -0500

    Refactor footer unit tests (#23)
    * refactor: add coverage report
    
    - add test coverage report directory
    - add unit test for App.tsx
    - remove unnecessary register lines from MyCarousel.tsx
    
    * refactor: add unit test for Footer
    
    - add register related lines for Swiper back in because they are necessary for Swiper to work
    - add unit tests for Footer
    - add state for user email addresses

--- client/src/components/ui/Footer/Footer.tsx
+++ client/src/components/ui/Footer/Footer.tsx
@@ -3,79 +3,93 @@ import "./Footer.css";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faFacebook,
-  faInstagram,
-  faTwitter,
-  faYoutube,
+    faFacebook,
+    faInstagram,
+    faTwitter,
+    faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-  const [submit, setSubmit] = useState(false);
-  const submitHandler = (e:FormEvent) => {
-    e.preventDefault();
-    setSubmit(true);
-  };
+    const [submit, setSubmit] = useState(false);
+    const [userEmail, setUserEmail] = useState("");
+    // TO DO: implement feature to store user emails in database
+    const submitHandler = (e: FormEvent) => { 
+        e.preventDefault();
+        setSubmit(true);
+        setUserEmail("");
+    };
 
-  return (
-    <footer>
-      <div className="footer-container">
-        <div className="communications-container">
-          <div className="newsletter-container">
-            <form
-              name="newsletter"
-              // method="POST"
-              className="newsletter__form"
-              onSubmit={(e) => {
-                submitHandler(e);
-              }}
-            >
-              <input type="hidden" name="newsletter" value="newsletter" />
+    return (
+        <footer>
+            <div className="footer-container">
+                <div className="communications-container">
+                    <div className="newsletter-container">
+                        <form
+                            name="newsletter"
+                            className="newsletter__form"
+                            onSubmit={(e) => {
+                                submitHandler(e);
+                            }}
+                        >
+                            <input
+                                type="hidden"
+                                name="newsletter"
+                                value="newsletter"
+                            />
+                            <div className="newsletter__label">
+                                <label>Sign up for our newsletter:</label>
+                            </div>
+                            <div className="newsletter__input-button">
+                                <input
+                                    type="email"
+                                    aria-label="userEmail"
+                                    placeholder="Enter your email address"
+                                    value={userEmail}
+                                    onChange={(e)=> {
+                                        setUserEmail(e.target.value)
+                                    }}
+                                    required
+                                ></input>
+                                <button type="submit">Submit</button>
+                                {submit && <p>Submitted!</p>}
+                            </div>
+                        </form>
+                    </div>
+                    <div className="socials-container">
+                        <div className="socials-title">
+                            <h3>Follow Us</h3>
+                        </div>
+                        <div className="socials-icons">
+                            <FontAwesomeIcon icon={faFacebook} />
+                            <FontAwesomeIcon icon={faInstagram} />
+                            <FontAwesomeIcon icon={faTwitter} />
+                            <FontAwesomeIcon icon={faYoutube} />
+                        </div>
+                    </div>
+                </div>
 
-              <div className="newsletter__label">
-                <label>Sign up for our newsletter:</label>
-              </div>
-              <div className="newsletter__input-button">
-                <input type="email" required></input>
-                <button type="submit">Submit</button>
-                {submit && <p>Submitted!</p>}
-              </div>
-            </form>
-          </div>
-          <div className="socials-container">
-            <div className="socials-title">
-              <h3>Follow Us</h3>
+                <div className="footer-elements">
+                    <ul>
+                        <li>
+                            <NavLink to={"/About"}>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="">FAQs</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="">News</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="">Careers</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="">Contact</NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="socials-icons">
-              <FontAwesomeIcon icon={faFacebook} />
-              <FontAwesomeIcon icon={faInstagram} />
-              <FontAwesomeIcon icon={faTwitter} />
-              <FontAwesomeIcon icon={faYoutube} />
-            </div>
-          </div>
-        </div>
-
-        <div className="footer-elements">
-          <ul>
-            <li>
-              <NavLink to={"/About"}>About</NavLink>
-            </li>
-            <li>
-              <NavLink to="">FAQs</NavLink>
-            </li>
-            <li>
-              <NavLink to="">News</NavLink>
-            </li>
-            <li>
-              <NavLink to="">Careers</NavLink>
-            </li>
-            <li>
-              <NavLink to="">Contact</NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </footer>
-  );
+        </footer>
+    );
 };
 
 export default Footer;
